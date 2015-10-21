07c6be168d0ffc7b5a67e8a37ab22c564d6336c7
robert-vo <robertvo79@gmail.com>
2015-10-21 00:39:09 -0500

    Added comments.

--- WeatherJsonExample/NSString+Utilities.m
+++ WeatherJsonExample/NSString+Utilities.m
@@ -10,21 +10,30 @@
 
 @implementation NSString (Utilities)
 
-+ (NSString *) createURL :(NSString*)city unit:(NSString*)unit{
+//Creates the url to use to contact the Open Weather Map API.
++ (NSString *) createURL :(NSString*)city unit:(NSString*)unit {
+
+    //API Key to use to connect to the Open Weather Map API.
     NSString *apiKey = @"&appid=e5e82b27942eb6b9b2e43e2b945e0704";
+    
+    //Units query
     NSString *temperatureUnit = @"&units=";
     
-    if([unit isEqualToString:@"Fahrenheit"]) {
+    if([unit isEqualToString:@"Fahrenheit"]) { //Imperial units
         temperatureUnit = [temperatureUnit stringByAppendingString:@"Imperial"];
     }
-    else if([unit isEqualToString:@"Celsius"]) {
+    else if([unit isEqualToString:@"Celsius"]) { //Metric units
         temperatureUnit = [temperatureUnit stringByAppendingString:@"Metric"];
     }
     else { //[unit isEqualToString:@"Kelvin
         temperatureUnit = [temperatureUnit stringByAppendingString:@""];
     }
+    
     NSString *beginningOfUrl = @"http://api.openweathermap.org/data/2.5/weather?q=";
+    
+    //Example return - http://api.openweathermap.org/data/2.5/weather?q=Houston&appid=e5e82b27942eb6b9b2e43e2b945e0704
     return [NSString stringWithFormat:@"%@%@%@%@", beginningOfUrl, city, apiKey, temperatureUnit];
+    
 }
 
 @end
